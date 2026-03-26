ba634981e0af787eacd2eb70d796661f56a111d5
lakshyaveyyakula <veyyakul@ualberta.ca>
2026-03-26 16:14:20 -0600

    Update model version from gemini-2.5 to gemini-2.0

--- app.py
+++ app.py
@@ -77,7 +77,7 @@ for msg in st.session_state.messages:
 
 
     response = client.models.generate_content(
-        model = "gemini-2.5-flash",
+        model = "gemini-2.0-flash",
         contents=conversation
     )
     reply = response.text
