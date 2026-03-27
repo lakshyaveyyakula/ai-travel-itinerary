7c19615971f0ccfa0a43ee620dcfc90c4e0fe975
lakshyaveyyakula <veyyakul@ualberta.ca>
2026-03-27 08:59:00 -0600

    Update app.py

--- app.py
+++ app.py
@@ -1,9 +1,11 @@
 import streamlit as st
 from google import genai
 import requests 
+from datetime import date
 
 st.set_page_config(page_title="AI Travel Planner", layout="centered")
-
+st.sidebar.header("Trip Timing")
+travel_date = st.sidebar.date_input("When are you visiting?", date.today())
 client = genai.Client(api_key=st.secrets["GEMINI_API_KEY"])
 
 def get_weather(city):
@@ -19,7 +21,25 @@ def get_weather(city):
         return f"{data['main']['temp']}°C and {data['weather'][0]['description']}"
     return "Weather data unavailable"
 
-    
+def get_events(city):
+    url = "https://serpapi.com/search.json"
+    params = {
+        "engine": "google_events",
+        "q": f"Events in {city} 2026",
+        "api_key": st.secrets["SERP_API_KEY"]
+    }
+    try:
+        r = requests.get(url, params=params)
+        if r.status_code == 200:
+            data = r.json()
+            events = data.get("events_results", [])
+            # Get title and date of first 3 events
+            event_list = [f"{e['title']} ({e.get('date', {}).get('start_date', 'Soon')})" for e in events[:3]]
+            return " | ".join(event_list) if event_list else "No major events found."
+        return "Event data unavailable"
+    except:
+        return "Could not connect to event service"
+        
 SYSTEM_PROMPT = """
 You are a travel assitant chatbot for suggesting places.
 
@@ -53,9 +73,9 @@ if user_input:
     st.chat_message("user").markdown(user_input)
 
     weather_info = get_weather(user_input)
-    
+    event_info = get_events(user_input)
 # Now Gemini knows if it's muggy, cold, or perfect for a hike!
-    conversation = SYSTEM_PROMPT + f"\n[REAL-TIME WEATHER]: {weather_info}\n"
+    conversation = SYSTEM_PROMPT + f"\n[REAL-TIME WEATHER]: {weather_info} | [EVENTS]: {event_info}\n"
     #conversation += f"[REAL-TIME EVENTS]: {event_info}\n"
     #conversation = SYSTEM_PROMPT + f"\n[CONTEXT] Weather: {weather_info} | Hotels: {hotel_info}\n"
     for msg in st.session_state.messages:
